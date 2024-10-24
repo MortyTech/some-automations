349f8cadefca42132aca2b92141d04ebabf1a4c7
MortyTech <78545071+MortyTech@users.noreply.github.com>
2024-10-24 04:48:58 +0330

    Update interface-portmap-ip.sh

--- interface-portmap-ip.sh
+++ interface-portmap-ip.sh
@@ -1,3 +1,8 @@
-for i in $(ls -l /sys/class/net/ | egrep 'vlan|bond|pci|br' | grep -v bonding_masters | awk {'print $9'}) ; do ip -br a | grep $i | awk '{print $1, $2, $3}' ; done | sort | uniq | sed "s/^/$HOSTNAME /" | sed 's/ /,/g' | tee /tmp/interface.txt
-for j in $(for i in $(ls -l /sys/class/net/   | egrep 'vlan|bond|pci|br' | grep -v bonding_masters | awk {'print $9'}) ; do ip -br a | grep $i | awk '{print $1, $2, $3}' ; done | sort | uniq | awk {'print $1'}) ; do echo $(sudo lldpcli show neighbors ports $j summary | grep PortDescr | awk {'print $2'}) $(sudo lldpcli show neighbors ports $j summary | grep SysName | awk {'print $2'}) ; done | tee /tmp/neighbor.txt
-paste -d ',' /tmp/interface.txt /tmp/neighbor.txt | sed 's/ /,/g' | sed '1i hostname,interface,status,IPv4,neighbor port,neighbor hostname' | tee ~/portmap.csv
+for i in $(ls -l /sys/class/net/ | egrep 'vlan|bond|pci|br' | grep -v bonding_masters | awk {'print $9'}) \
+; do ip -br a | grep $i | awk '{print $1, $2, $3}' \
+; done | sort | uniq | sed "s/^/$HOSTNAME /" | sed 's/ /,/g' | tee /tmp/interface.txt
+for j in $(for i in $(ls -l /sys/class/net/ | egrep 'vlan|bond|pci|br' | grep -v bonding_masters | awk {'print $9'}) \
+; do ip -br a | grep $i | awk '{print $1, $2, $3}' ; done | sort | uniq | awk {'print $1'}) \
+; do echo $(sudo lldpcli show neighbors ports $j summary | grep PortDescr | awk {'print $2'}) $(sudo lldpcli show neighbors ports $j summary | grep SysName | awk {'print $2'}) ; done | tee /tmp/neighbor.txt
+paste -d ',' /tmp/interface.txt /tmp/neighbor.txt | sed 's/ /,/g' | \
+sed '1i hostname,interface,status,IPv4,neighbor port,neighbor hostname' | tee ~/portmap.csv
